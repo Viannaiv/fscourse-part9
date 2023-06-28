9c3d011b17a7c0cd9b950a9a9d7d21f89af5ea2f
Viannaiv <viva95@windowslive.com>
2023-06-28 16:31:40 +0300

    Exercise 9.11

---
+++ patientor_backend/src/services/patientService.ts
@@ -0,0 +1,23 @@
+import data from '../../data/patients';
+import { Patient, NonSensitivePatient } from '../types';
+
+const patients: Patient[] = data;
+
+const getPatients = (): Patient[] => {
+  return patients;
+};
+
+const getNonSensitivePatients = (): NonSensitivePatient[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation
+  }));
+};
+
+export default {
+  getPatients,
+  getNonSensitivePatients
+};
